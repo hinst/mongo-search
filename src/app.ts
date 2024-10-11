0450fe16928608fb1b30177ebee29655fcdb49c0
hinst <hinstance@yandex.ru>
2024-10-11 14:19:18 +0200

    Improvements

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,25 @@ import { MongoClient } from 'mongodb';
 
 async function main() {
 	const dbName = process.argv[2];
-	const desiredText = process.argv[3].toLowerCase();
+	const desiredText = process.argv[3];
+	const lowerCaseDesiredText = desiredText.toLowerCase();
 	const client = await new MongoClient('mongodb://localhost:27017').connect();
 	try {
 		const db = client.db(dbName);
 		const collections = await db.collections();
-		for (const collectionInfo of collections) {
+		for (let i = 0; i < collections.length; i++) {
+			const collectionInfo = collections[i];
+			console.log('Searching collection ' + collectionInfo.collectionName + '[' + i + '/' + collections.length + ']');
 			const collection = db.collection(collectionInfo.collectionName);
 			for (
 				const documents = collection.find({});
 				await documents.hasNext();
 			) {
 				const document = await documents.next();
-				const text = JSON.stringify(document).toLowerCase();
-				if (text.includes(desiredText)) {
-					console.log(`Found in ${collectionInfo.collectionName}`);
-					console.log(document);
+				const text = JSON.stringify(document);
+				const lowerCaseText = JSON.stringify(document).toLowerCase();
+				if (lowerCaseText.includes(lowerCaseDesiredText)) {
+					console.log(`Found in ${collectionInfo.collectionName} [${document?._id}] ${text.slice(0, 100)}`);
 				}
 			}
 		}
